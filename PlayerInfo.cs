5ed6c7ce682339799f647e9c32d4c8979df95d9f
KobiSol <kobi.m@solocate.com>
2021-10-21 14:54:14 +0300

    UpdatePlayerInfo

--- PlayerInfo.cs
+++ PlayerInfo.cs
@@ -39,13 +39,12 @@ namespace CreateEspnDBFile
 
         private Player UpdatePlayerInfo(string playerStr, int id)
         {
-            string pattern = "<script type='text/javascript' >";
+            string pattern = "{\"app\"";
             string pattern2 = ";</script>";
             var i1 = playerStr.IndexOf(pattern);
-            var i2 = playerStr.IndexOf(pattern, i1 + 30);
-            var i3 = playerStr.IndexOf(pattern, i2 + 30);
+            var i2 = playerStr.IndexOf(pattern2);
             if (i1 == -1) return null;
-            string jsonStr = playerStr.Substring(i2 + 55, i3 - i2 - 70).TrimEnd().Replace(pattern2, "");
+            string jsonStr = playerStr.Substring(i1, i2 - i1);
             JObject json = JObject.Parse(jsonStr);
             JToken playerInfo = json["page"]["content"]["player"]["plyrHdr"]["ath"];
 
